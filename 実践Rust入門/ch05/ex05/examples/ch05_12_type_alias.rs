4e4f308f0c103d5e78f37d44833897385d5ef6ec
masaki <kashiwabara356@gmail.com>
2020-05-20 02:07:47 +0900

    add ch05 type_alias

---
+++ 実践Rust入門/ch05/ex05/examples/ch05_12_type_alias.rs
@@ -0,0 +1,16 @@
+type UserName = String;
+type Id = i64;
+type Timestamp = i64;
+type  User = (Id, UserName, Timestamp);
+
+fn new_user(name: UserName, id: Id, created: Timestamp) -> User {
+    (id, name, created)
+}
+
+fn main() {
+    let id = 400;
+    let now = 4567890123;
+    let user = new_user(String::from("mika"), id, now);
+
+    let bad_user = new_user(String::from("kazuki"), now, id);
+}
