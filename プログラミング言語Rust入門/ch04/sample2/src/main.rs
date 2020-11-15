fcebc71dba7a86d10f2bbb108d755a095c813133
masaki4423 <kashiwabara356@gmail.com>
2020-11-16 02:12:22 +0900

    add ch04(WIP)

--- プログラミング言語Rust入門/ch04/sample2/src/main.rs
+++ プログラミング言語Rust入門/ch04/sample2/src/main.rs
@@ -28,4 +28,48 @@ fn main() {
     println!("s is {}", s);
     let s = String::from("hello rust world.");
     println!("s is {}", s);
+
+    let s = "こんにちは rsut コードの世界";
+    println!("s is {}", s);
+
+    let s = "こんにちは rsut コードの世界";
+    // let hello = &s[0..5];
+    // let world = &s[11..];
+    let hello = &s[0..15];
+    let world = &s[21..];
+    println!("こんにちは is {}", hello);
+    println!("コードの世界 is {}", world);
+
+    let s = "こんにちは rust コードの世界";
+    let len = s.len();
+    println!("s.len is {}", len);
+
+    let mut s = String::new();
+    s.push_str("こんにちは ");
+    s.push_str("rust ");
+    s.push_str("コードの世界");
+    println!("s is {}", s);
+
+    let hello = "こんにちは";
+    let rust = "RUST";
+    let world = "コードの世界";
+    let s = format!("{} {} {}", hello, rust, world);
+    println!("s is {}", s);
+
+    let s = "こんにちは rust コードの世界".to_string();
+    println!("s is {}", s);
+    let s = String::from("こんにちは rust コードの世界");
+    println!("s is {}", s);
+
+    let s = "This is ねこ🐱neko 文字列";
+    let mut v : Vec<char> = Vec::new();
+    for c in s.chars() {
+        v.push(c);
+    }
+    let v = &v[8..15];
+    let mut s = String::new();
+    for c in v {
+        s.push(*c)
+    }
+    println!("s is {}", s);
 }
