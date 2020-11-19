fd64d9104a50174e293a7cae679960b7860f1834
masaki4423 <kashiwabara356@gmail.com>
2020-11-20 01:48:41 +0900

    add ch06(WIP)

---
+++ プログラミング言語Rust入門/ch06/sample1/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let a = 10;
+    let b = 20;
+    if a < b {
+        println!("a < b is ok.");
+    }
+
+    // Warning
+    // let a = 10;
+    // let b = 20;
+    // if ( a < b ) {
+    //     println!("a < b is ok.");
+    // }
+
+    let a = 10;
+    let b = 20;
+    if a < b {
+        println!("a < b is ok.");
+    } else {
+        println!("a < b is ng.");
+    }
+
+    if a < b {
+
+    } else {
+        println!("a < b is ng.");
+    }
+}
