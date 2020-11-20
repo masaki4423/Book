ef57a591f6626f5c6c417375ee490c36638072a8
masaki4423 <kashiwabara356@gmail.com>
2020-11-21 03:47:50 +0900

    add ch06(WIP)

--- プログラミング言語Rust入門/ch06/sample1/src/main.rs
+++ プログラミング言語Rust入門/ch06/sample1/src/main.rs
@@ -25,4 +25,43 @@ fn main() {
     } else {
         println!("a < b is ng.");
     }
+
+    let a = 10;
+    let b = 20;
+    if a == b {
+        println!("a == b is ok.");
+    } else if a < b {
+        println!("a < b is ok.");
+    } else {
+        println!("a > b is ok.");
+    }
+
+    let a = 10;
+    let b = 20;
+    if a == 10 && b == 20 {
+        println!("AND is ok.");
+    }
+    if a == 10 || b == 20 {
+        println!("OR is ok.");
+    }
+
+    let a = 10;
+    let b = 20;
+    if test(a, b) {
+        println!("test is ok.");
+    }
+
+    let a = 10;
+    let b = 20;
+    let x = if a < b {1} else {0};
+    println!("x is {}", x);
+
+    // let a = 10;
+    // let b = 20;
+    // let x = if a < b {1} else {"hello"};
+    // println!("x is {}", x);
+}
+
+fn test(x: i32, y: i32) -> bool {
+    x < y
 }
