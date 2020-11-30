7cb0980bf21fdb2af6a0a3d097cc261e4642418f
masaki4423 <kashiwabara356@gmail.com>
2020-11-30 11:40:55 +0900

    add ch08(WIP)

--- プログラミング言語Rust入門/ch08/sample1/src/main.rs
+++ プログラミング言語Rust入門/ch08/sample1/src/main.rs
@@ -14,4 +14,20 @@ fn main() {
         age: 50,
         addr: String::from("Tokyo"),
     };
+
+    let mut pa = Person {
+        id: 100,
+        name: String::from("masuda"),
+        age: 50,
+        addr: String::from("Tokyo"),
+    };
+
+    println!("{}: {} ({}) in {}",
+        pa.id, pa.name, pa.age, pa.addr);
+    pa.age += 1;
+    println!("{}: {} ({}) in {}",
+        pa.id, pa.name, pa.age, pa.addr);
+    pa.addr = String::from("Osaka");
+    println!("{}: {} ({}) in {}",
+        pa.id, pa.name, pa.age, pa.addr);
 }
