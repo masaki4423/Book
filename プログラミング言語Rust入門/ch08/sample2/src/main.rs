e356a0f52aaf2b27c9741548386dd7a20d064ec5
masaki4423 <kashiwabara356@gmail.com>
2020-12-02 15:44:29 +0900

    add ch08(WIP)

---
+++ プログラミング言語Rust入門/ch08/sample2/src/main.rs
@@ -0,0 +1,23 @@
+struct Person {
+    id: i32,
+    name: String,
+    age: i32,
+    addr: String,
+}
+
+impl Person {
+    fn print(&self) {
+        println!("{}: {} ({}) in {}",
+            self.id, self.name, self.age, self.addr);
+    }
+}
+
+fn main() {
+    let pa = Person {
+        id: 1,
+        name: String::from("masuda"),
+        age: 50,
+        addr: String::from("Tokyo"),
+    };
+    pa.print();
+}
