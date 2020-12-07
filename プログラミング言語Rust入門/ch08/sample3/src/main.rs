5e01ec9b36445525edbb3fb757ef05eb05579fd8
masaki4423 <kashiwabara356@gmail.com>
2020-12-07 12:38:16 +0900

    add ch08(WIP)

--- プログラミング言語Rust入門/ch08/sample3/src/main.rs
+++ プログラミング言語Rust入門/ch08/sample3/src/main.rs
@@ -1,3 +1,5 @@
+use std::num::ParseIntError;
+
 fn main() {
     let r = "100".parse::<i32>();
     match r {
@@ -14,6 +16,29 @@ fn main() {
     let n = "100".parse::<i32>().unwrap();
     println!("n is {}", n);
 
-    let n = "xxx".parse::<i32>().unwrap();
-    println!("n is {}", n);
+    // let n = "xxx".parse::<i32>().unwrap();
+    // println!("n is {}", n);
+
+    // let n: i32 = half_number("100");
+    // println!("n is {}", n);
+
+    // let n: i32 = half_number("xxx");
+    // println!("n is {}", n);
+
+    match half_number("100") {
+        Ok(n) => println!("Ok: {}", n),
+        Err(err) => println!("Error: {:?}", err),
+    }
+
+    match half_number("xxx") {
+        Ok(n) => println!("Ok: {}", n),
+        Err(err) => println!("Error: {:?}", err),
+    }
+}
+
+fn half_number(s: &str) -> Result<i32, ParseIntError> {
+    match s.parse::<i32>() {
+        Ok(n) => Ok(n / 2),
+        Err(err) => Err(err),
+    }
 }
