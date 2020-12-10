fa35b3189ae1d95f1a7a8d5aa43a72dd6bbe8c49
masaki4423 <kashiwabara356@gmail.com>
2020-12-11 02:02:48 +0900

    add ch09(WIP)

---
+++ プログラミング言語Rust入門/ch09/sample2/src/main.rs
@@ -0,0 +1,55 @@
+struct Rectange {
+    width: f32,
+    height: f32,
+}
+
+struct Triangle {
+    base: f32,
+    height: f32,
+}
+
+struct Circle {
+    redius: f32
+}
+
+trait CalcArea {
+    fn calc_area(&self) -> f32;
+}
+
+impl CalcArea for Rectange {
+    fn calc_area(&self) -> f32 {
+        self.width * self.height
+    }
+}
+
+impl CalcArea for Triangle {
+    fn calc_area(&self) -> f32 {
+        self.base * self.height * 0.5
+    }
+}
+
+impl CalcArea for Circle {
+    fn calc_area(&self) -> f32 {
+        self.redius * self.redius * 3.14
+    }
+}
+
+fn main() {
+    let rect = Rectange {
+        width: 10.0,
+        height: 20.0,
+    };
+
+    let tri = Triangle {
+        base: 10.0,
+        height: 20.0,
+    };
+
+    let cir = Circle {
+        redius: 10.0
+    };
+
+    println!("rect area is {}", rect.calc_area());
+    println!("tri area is {}", tri.calc_area());
+    println!("cir area is {}", cir.calc_area());
+}
