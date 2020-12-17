db2a8e969bc8bfc490345d8d81a50d0d85932134
masaki4423 <kashiwabara356@gmail.com>
2020-12-17 21:37:46 +0900

    add ch10(WIP)

---
+++ プログラミング言語Rust入門/ch10/sample1/src/main.rs
@@ -0,0 +1,15 @@
+#[derive(Debug)]
+struct Person {
+    name: &'static str,
+    age: i32,
+}
+
+fn print_a(a: &Person) {
+    println!("print_a: a is {:?}", a);
+}
+
+fn main() {
+    let a = Person {name: "masuda", age: 50};
+    print_a(&a);
+    println!("main: a is {:?}", a);
+}
