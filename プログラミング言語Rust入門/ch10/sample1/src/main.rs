109c37f0ef729b0fe782428ce0a2d108e8910a69
masaki4423 <kashiwabara356@gmail.com>
2020-12-18 01:01:27 +0900

    add ch10(WIP)

--- プログラミング言語Rust入門/ch10/sample1/src/main.rs
+++ プログラミング言語Rust入門/ch10/sample1/src/main.rs
@@ -8,8 +8,16 @@ fn print_a(a: &Person) {
     println!("print_a: a is {:?}", a);
 }
 
+fn move_a(a: Person) {
+    println!("move_a: a is {:?}", a);
+}
+
 fn main() {
     let a = Person {name: "masuda", age: 50};
     print_a(&a);
     println!("main: a is {:?}", a);
+
+    let a = Person {name: "masuda", age: 50};
+    move_a(a);
+    // println!("main: a is {:?}", a);
 }
