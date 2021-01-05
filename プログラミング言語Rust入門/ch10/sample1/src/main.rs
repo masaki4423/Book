df1daa63f69516c871aa1b057ff69710866d088a
masaki4423 <kashiwabara356@gmail.com>
2021-01-05 11:59:23 +0900

    add ch10(WIP)

--- プログラミング言語Rust入門/ch10/sample1/src/main.rs
+++ プログラミング言語Rust入門/ch10/sample1/src/main.rs
@@ -91,4 +91,16 @@ fn main() {
     // let y = a;
     let y = &a;
     println!("y is {}", y);
+
+    let a = Person {name: "masuda", age: 50};
+    println!("a is {:?}", a);
+    let x = &a;
+    println!("変数xが借用する");
+    println!("x is {:?}", x);
+    println!("a is {:?}", a);
+    let y = a;
+    println!("変数yに所有権を移す");
+    println!("y is {:?}", y);
+    // println!("a is {:?}", a);
+    // println!("x is {:?}", x);
 }
