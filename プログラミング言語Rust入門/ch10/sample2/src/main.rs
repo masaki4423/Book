c8ead04c10192c2b89760d6b0d95796303646b92
masaki4423 <kashiwabara356@gmail.com>
2021-01-08 00:15:34 +0900

    add ch10(WIP)

--- プログラミング言語Rust入門/ch10/sample2/src/main.rs
+++ プログラミング言語Rust入門/ch10/sample2/src/main.rs
@@ -15,4 +15,17 @@ fn main() {
         x = a;
     }
     println!("x is {:?}", x);
+
+    let a = new_person("masuda", 50);
+    println!("a is {:?}", a);
+}
+
+// fn new_person(id: i32, name: String) -> &Person {
+fn new_person(name: &str, age: i32) -> Person {
+    let p = Person {
+        name: String::from(name),
+        age: age,
+    };
+    // &p
+    p
 }
