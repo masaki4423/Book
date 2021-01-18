0e008aa03a2ee018e40303505af7093b3119b24a
masaki4423 <kashiwabara356@gmail.com>
2021-01-18 22:14:20 +0900

    add ch12(WIP)

---
+++ プログラミング言語Rust入門/ch12/sample1/src/main.rs
@@ -0,0 +1,48 @@
+use std::fs;
+use std::fs::*;
+
+use std::fs::File;
+use std::io::Read;
+fn main() {
+    let path = "sample.txt";
+    println!("read all lines.");
+    if let Ok(data) = std::fs::read_to_string(path) {
+        println!("data is {}", data);
+    }
+
+    let path = "sample.txt";
+    println!("read all lines.");
+    if let Ok(data) = fs::read_to_string(path) {
+        println!("data is {}", data);
+    }
+
+    let path = "sample.txt";
+    println!("read all lines.");
+    if let Ok(data) = read_to_string(path) {
+        println!("data is {}", data);
+    }
+
+    let path = "unknown.txt";
+    println!("read all lines.");
+    if let Ok(data) = fs::read_to_string(path) {
+        println!("data is {}", data);
+    } else {
+        println!("cannot open {}", path);
+    }
+
+    let path = "unknown.txt";
+    println!("read all lines.");
+    match fs::read_to_string(path) {
+        Ok(data) => { println!("data is {}", data) },
+        _ => { println!("cannot open {}", path) }
+    }
+
+    let path = "sample.txt";
+    println!("read all lines by buffer.");
+    let mut file = File::open(path)
+        .expect("file not found.");
+    let mut data = String::new();
+    file.read_to_string(&mut data)
+        .expect("read error");
+    println!("data is {}", data);
+}
