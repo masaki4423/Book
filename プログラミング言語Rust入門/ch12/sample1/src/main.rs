0634ca2d7947ad570c8194184943b02f097ca24e
masaki4423 <kashiwabara356@gmail.com>
2021-01-21 21:47:47 +0900

    add ch12(WIP)

--- プログラミング言語Rust入門/ch12/sample1/src/main.rs
+++ プログラミング言語Rust入門/ch12/sample1/src/main.rs
@@ -45,4 +45,13 @@ fn main() {
     file.read_to_string(&mut data)
         .expect("read error");
     println!("data is {}", data);
+
+    let path = "sample.txt";
+    println!("read all lines by buffer.");
+    if let Ok(mut file) = File::open(path) {
+        let mut data = String::new();
+        if let Ok(_) = file.read_to_string(&mut data) {
+            println!("data is {}", data);
+        }
+    }
 }
