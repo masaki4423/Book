36c98a5bb4fa076f91f22c62ea580f7e58edff68
masaki4423 <kashiwabara356@gmail.com>
2021-01-22 22:24:15 +0900

    add ch12(WIP)

--- プログラミング言語Rust入門/ch12/sample1/src/main.rs
+++ プログラミング言語Rust入門/ch12/sample1/src/main.rs
@@ -54,4 +54,14 @@ fn main() {
             println!("data is {}", data);
         }
     }
+
+    let path = "sample.txt";
+    println!("read 16 bytes by buffer.");
+    let mut file = File::open(path)
+        .expect("fie not found");
+    let mut buf: [u8; 1] = [0; 1];
+    for i in 0..16 {
+        file.read(&mut buf);
+        println!("buf is {}: {}", i, buf[0] as char);
+    }
 }
