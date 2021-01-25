b8bbef4d8e433983c9ea8a5a777eec2986ed2635
masaki4423 <kashiwabara356@gmail.com>
2021-01-25 21:51:32 +0900

    add ch12(WIP)

--- プログラミング言語Rust入門/ch12/sample1/src/main.rs
+++ プログラミング言語Rust入門/ch12/sample1/src/main.rs
@@ -3,6 +3,7 @@ use std::fs::*;
 
 use std::fs::File;
 use std::io::Read;
+use std::io::{BufRead, BufReader};
 fn main() {
     let path = "sample.txt";
     println!("read all lines.");
@@ -63,5 +64,14 @@ fn main() {
     for i in 0..16 {
         file.read(&mut buf);
         println!("buf is {}: {}", i, buf[0] as char);
+    }
+
+    println!("read every one line.");
+    let file = File::open(path)
+        .expect("file not found");
+    for line in BufReader::new(file).lines() {
+        if let Ok(l) = line {
+            println!("line is {}", l);
+        }
     }
 }
