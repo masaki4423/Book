8bdf4de8c322a447c58c53446e4683b2402d7910
masaki4423 <kashiwabara356@gmail.com>
2021-01-28 20:56:29 +0900

    add ch12(WIP)

---
+++ プログラミング言語Rust入門/ch12/sample2/src/main.rs
@@ -0,0 +1,10 @@
+use std::fs::File;
+use std::io::Write;
+
+fn main() {
+    let path = "sample.txt";
+    let mut file = File::create(path)
+        .expect("file not found.");
+    writeln!(file, "hello rust world")
+        .expect("cannot write.");
+}
