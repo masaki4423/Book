0cbe1c78065818ff63bbb5d5941b87cc80ca99a5
masaki4423 <kashiwabara356@gmail.com>
2021-02-05 21:30:20 +0900

    add ch12(WIP)

---
+++ プログラミング言語Rust入門/ch12/sample3/src/main.rs
@@ -0,0 +1,18 @@
+use std::fs::File;
+use std::io::{Read, Write, BufReader, BufWriter};
+use std::env;
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() <= 1 {
+
+    } else {
+        let file = File::open(&args[1])
+            .expect("file not found.");
+        let reader = BufReader::new(file);
+        let mut writer = BufWriter::new(std::io::stdout());
+        for it in reader.bytes() {
+            writer.write(&[*it]);
+        }
+    }
+}
