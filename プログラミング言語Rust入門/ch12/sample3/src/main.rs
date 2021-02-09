93d1d83b2b95114745cc6139d513a85c8c595aa2
masaki4423 <kashiwabara356@gmail.com>
2021-02-09 21:38:13 +0900

    add ch12(WIP)

--- プログラミング言語Rust入門/ch12/sample3/src/main.rs
+++ プログラミング言語Rust入門/ch12/sample3/src/main.rs
@@ -1,17 +1,21 @@
-use std::fs::File;
+use std::{fs::File, io::{stdin, stdout}};
 use std::io::{Read, Write, BufReader, BufWriter};
 use std::env;
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
     if args.len() <= 1 {
-
+        let reader = BufReader::new(std::io::stdin());
+        let mut writer = BufWriter::new(std::io::stdout());
+        for it in reader.bytes(){
+            writer.write(&[*it]);
+        }
     } else {
         let file = File::open(&args[1])
             .expect("file not found.");
         let reader = BufReader::new(file);
         let mut writer = BufWriter::new(std::io::stdout());
-        for it in reader.bytes() {
+        for it in reader.bytes(){
             writer.write(&[*it]);
         }
     }
