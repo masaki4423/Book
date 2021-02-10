75058d6c895295fb3ce9e78859bffa636b7e4261
masaki4423 <kashiwabara356@gmail.com>
2021-02-10 21:03:55 +0900

    add ch12

--- プログラミング言語Rust入門/ch12/sample3/src/main.rs
+++ プログラミング言語Rust入門/ch12/sample3/src/main.rs
@@ -6,17 +6,26 @@ fn main() {
     let args = env::args().collect::<Vec<String>>();
     if args.len() <= 1 {
         let reader = BufReader::new(std::io::stdin());
-        let mut writer = BufWriter::new(std::io::stdout());
-        for it in reader.bytes(){
-            writer.write(&[*it]);
-        }
+        // let mut writer = BufWriter::new(std::io::stdout());
+        // for it in reader.bytes(){
+            // writer.write(&[*it]);
+        // }
+        do_print(reader);
     } else {
         let file = File::open(&args[1])
             .expect("file not found.");
         let reader = BufReader::new(file);
-        let mut writer = BufWriter::new(std::io::stdout());
-        for it in reader.bytes(){
-            writer.write(&[*it]);
-        }
+        // let mut writer = BufWriter::new(std::io::stdout());
+        // for it in reader.bytes(){
+            // writer.write(&[*it]);
+        // }
+        do_print(reader);
+    }
+}
+
+fn do_print<R>(reader: BufReader<R>) where R: std::io::Read{
+    let mut writer = BufWriter::new(std::io::stdout());
+    for it in reader.bytes(){
+        writer.write(&[*it]);
     }
 }
