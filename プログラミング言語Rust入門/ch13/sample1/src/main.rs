a9afa420a698e16f527859f29595f26b68bea630
masaki4423 <kashiwabara356@gmail.com>
2021-02-22 23:27:21 +0900

    add ch13(WIP)

--- プログラミング言語Rust入門/ch13/sample1/src/main.rs
+++ プログラミング言語Rust入門/ch13/sample1/src/main.rs
@@ -24,4 +24,11 @@ fn main() {
     println!("16進数 {:X}", n);
     println!(" 8進数 {:o}", n);
     println!(" 2進数 {:b}", n);
+
+    let n = 10;
+    println!("通常    {}", n);
+    println!("桁揃え {:4}", n);
+    println!("10進数 {:04}", n);
+    println!("16進数 {:02X}", n);
+    println!(" 2進数 {:08b}", n);
 }
