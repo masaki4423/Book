9061bbf51cd7079a37c2530a25d4a981352c0916
masaki4423 <kashiwabara356@gmail.com>
2021-02-23 22:03:52 +0900

    add ch13(WIP)

--- プログラミング言語Rust入門/ch13/sample1/src/main.rs
+++ プログラミング言語Rust入門/ch13/sample1/src/main.rs
@@ -31,4 +31,15 @@ fn main() {
     println!("10進数 {:04}", n);
     println!("16進数 {:02X}", n);
     println!(" 2進数 {:08b}", n);
+
+    let f = 10.234;
+    println!("{} ", f);
+    println!("{:e} ", f);
+    println!("{:E} ", f);
+    println!("{:.2}", f);
+    let f = 0.0234;
+    println!("{} ", f);
+    println!("{:e} ", f);
+    println!("{:E} ", f);
+    println!("{:.2}", f);
 }
