eb79649d801733a2abbc0c8d687c9f384e0c2acb
masaki4423 <kashiwabara356@gmail.com>
2021-03-13 20:33:34 +0900

    add ch13(WIP)

--- プログラミング言語Rust入門/ch13/sample2/src/main.rs
+++ プログラミング言語Rust入門/ch13/sample2/src/main.rs
@@ -1,7 +1,31 @@
+#[derive(Debug)]
+struct Person {
+    id: i32,
+    name: String,
+    age: i32,
+    addr: String,
+}
+
 fn main() {
     let a = [1, 2, 3, 4, 5];
     println!("a is {:?}", a);
 
     let a = [1, 2, 3, 4, 5];
     dbg!(a);
+
+    let p = Person {
+        id: 100,
+        name: String::from("masuda"),
+        age: 50,
+        addr: String::from("Tokyo"),
+    };
+    dbg!(p);
+
+    let p = Person {
+        id: 100,
+        name: String::from("masuda"),
+        age: 50,
+        addr: String::from("Tokyo"),
+    };
+    println!("p is {:?}", p);
 }
